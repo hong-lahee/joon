83c6ad21707d6f1615959ff33ffa5dbc2ec59e43
hong-lahee <xhfxhfldwlsk@naver.com>
2025-05-27 18:20:50 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,14 @@
+i = input()
+i = i.split(' ')
+a = i[0]
+b = i[1]
+c = int(a) + int(b)
+d = int(a) - int(b)
+e = int(a) * int(b)
+f = int(a) // int(b)
+g = int(a) % int(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
