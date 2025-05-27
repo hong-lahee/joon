a514ad48167152d51d3cf9f4e7312cfe117e679d
hong-lahee <xhfxhfldwlsk@naver.com>
2025-05-27 18:16:43 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+a = input()
+b = input()
+a = int(a)
+b0 = int(b[0])
+b1 = int(b[1])
+b2 = int(b[2])
+c = a *b2
+c1 = a* b1
+c2 = a* b0
+c3 = c + (c1* 10) + (c2*100)
+print(c)
+print(c1)
+print(c2)
+print(c3)
