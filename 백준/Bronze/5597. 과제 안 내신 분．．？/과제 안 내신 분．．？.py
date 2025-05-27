2521f1a8ecd74f15402f1de6f4e36c0314c17d1c
hong-lahee <xhfxhfldwlsk@naver.com>
2025-05-27 18:19:04 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,14 @@
+i = []
+for c in range(28):
+    num =int(input())
+    i.append(num)
+s =[]
+for a in range(1,31):
+    if a not in i:
+        s.append(a)
+for p in s:
+    print(p)
+    
+    
+  
+  
