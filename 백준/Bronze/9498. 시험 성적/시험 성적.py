e43ba586c73f75bb816d112797ee7b3d908008de
hong-lahee <xhfxhfldwlsk@naver.com>
2025-05-27 18:19:36 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+i = int(input())
+
+if i >= 90:
+    print('A')
+elif i >= 80 and i <= 89:
+    print('B')
+elif i >= 70 and i <= 79:
+    print('C')
+elif i >= 60 and i <= 69:
+    print('D')
+else:
+    print('F')
